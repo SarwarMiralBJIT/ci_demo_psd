bf137e99d49dedfbcf47b117f764f3891fd14bb8
Sarwar Miral <sarwar.miral@bjitgroup.com>
2024-08-28 17:44:54 +0600

    Added Logger

--- src/main/java/com/example/ci_demo_psd/CiDemoPsdApplication.java
+++ src/main/java/com/example/ci_demo_psd/CiDemoPsdApplication.java
@@ -1,12 +1,16 @@
 package com.example.ci_demo_psd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CiDemoPsdApplication {
 
+	static Logger logger = LoggerFactory.getLogger(CiDemoPsdApplication.class);
 	public static void main(String[] args) {
+		logger.info("This is a running job.");
 		SpringApplication.run(CiDemoPsdApplication.class, args);
 	}
 
