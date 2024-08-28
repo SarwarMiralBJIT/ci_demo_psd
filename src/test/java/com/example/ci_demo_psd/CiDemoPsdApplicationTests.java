227bd62205ad81573cd12621e3804689de31c0cc
Sarwar Miral <sarwar.miral@bjitgroup.com>
2024-08-28 17:56:53 +0600

    Added Tests

--- src/test/java/com/example/ci_demo_psd/CiDemoPsdApplicationTests.java
+++ src/test/java/com/example/ci_demo_psd/CiDemoPsdApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.ci_demo_psd;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiDemoPsdApplicationTests {
 
+	static Logger logger = LoggerFactory.getLogger(CiDemoPsdApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("It's the first test!");
+		assertEquals(true, true);
+	}
+
+	@Test
+	void demoTest() {
+		logger.info("It's the second test!");
+		assertEquals(true, true);
 	}
 
 }
